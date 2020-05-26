460a6ccae226ab50d21d7d2af28f3455b94d37ec
Steven Del Rosario <stevendelro@pm.me>
2020-05-26 10:50:41 -0700

    initial setup for material-ui usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,38 @@
+import { useEffect } from 'react'
+import Head from 'next/head';
 import App from 'next/app'
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import theme from '../styles/theme/theme';
 import { wrapper } from '../store/store'
 
 class WrappedApp extends App {
   render() {
     const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+
+    useEffect(() => {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles) {
+        jssStyles.parentElement.removeChild(jssStyles);
+      }
+    }, [])
+
+    return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  )
   }
 }
 
