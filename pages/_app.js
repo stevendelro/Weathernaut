0375ecb1bdcadda2687221baaff209156eee16b7
Steven Del Rosario <stevendelro@pm.me>
2020-05-28 18:33:35 -0700

    fix: componentDidMount instead of useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,45 @@
 import { useEffect } from 'react'
-import Head from 'next/head';
+import Head from 'next/head'
 import App from 'next/app'
 
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-import theme from '../styles/theme/theme';
+import theme from '../styles/theme/theme'
 import { wrapper } from '../store/store'
 
 class WrappedApp extends App {
+  constructor(props) {
+    super(props)
+  }
+  // Remove the server-side injected CSS. This is necessary for Material-UI.
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }
   render() {
     const { Component, pageProps } = this.props
 
-    useEffect(() => {
-      // Remove the server-side injected CSS.
-      const jssStyles = document.querySelector('#jss-server-side');
-      if (jssStyles) {
-        jssStyles.parentElement.removeChild(jssStyles);
-      }
-    }, [])
-
     return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <div style={{ display: 'flex '}}>
-          <Component {...pageProps} />
-        </div>
-
-      </ThemeProvider>
-    </React.Fragment>
-  )
+      <React.Fragment>
+        <Head>
+          <title>My page</title>
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
+          />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <div style={{ display: 'flex ' }}>
+            <Component {...pageProps} />
+          </div>
+        </ThemeProvider>
+      </React.Fragment>
+    )
   }
 }
 
