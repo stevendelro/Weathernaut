862a1abfe188d7bd4cf44a0e8634e6f389bd04b6
Steven Del Rosario <stevendelro@pm.me>
2020-05-30 19:16:43 -0700

    converted _app to a functional component

--- pages/_app.js
+++ pages/_app.js
@@ -8,39 +8,34 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '../styles/theme/theme'
 import { wrapper } from '../store/store'
 
-class WrappedApp extends App {
-  constructor(props) {
-    super(props)
-  }
-  // Remove the server-side injected CSS. This is necessary for Material-UI.
-  componentDidMount() {
+function MyApp(props) {
+  const { Component, pageProps } = props
+  useEffect(() => {
+    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
-  }
-  render() {
-    const { Component, pageProps } = this.props
+  }, [])
 
-    return (
-      <React.Fragment>
-        <Head>
-          <title>My page</title>
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width'
-          />
-        </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <div style={{ display: 'flex ' }}>
-            <Component {...pageProps} />
-          </div>
-        </ThemeProvider>
-      </React.Fragment>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <div style={{ display: 'flex ' }}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default wrapper.withRedux(WrappedApp)
+export default wrapper.withRedux(MyApp)
