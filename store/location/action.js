5df90346ab1b0d910f8997a0a6c62b05ef00bbf1
Steven Del Rosario <stevendelro@pm.me>
2020-05-31 16:56:14 -0700

    removed useSWR for geolocation dependant API calls

--- store/location/action.js
+++ store/location/action.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import useSWR from 'swr'
 import fetcher from '../../util/fetcher'
 import { capitalizeFirstLetter } from '../../util/capitalizeFirstLetter'
@@ -6,41 +7,70 @@ export const setLocationActionTypes = {
   SET_LOCATION: 'SET_LOCATION',
 }
 
-export const setLocationByCoords = coords => dispatch => {
-  try {
-    useSWR(`/api/mapbox/${coords[0]}/${coords[1]}`, fetcher, {
-      onSuccess: ({ data }) => {
-        dispatch({
-          type: setLocationActionTypes.SET_LOCATION,
-          payload: data,
-        })
-      },
-      shouldRetryOnError: false,
+// export const getLocationByCoords = coords => dispatch => {
+//   try {
+//     useSWR(`/api/mapbox/${coords[0]}/${coords[1]}`, fetcher, {
+//       onSuccess: ({ data }) => {
+//         dispatch({
+//           type: setLocationActionTypes.SET_LOCATION,
+//           payload: data,
+//         })
+//       },
+//       shouldRetryOnError: false,
+//     })
+//   } catch (error) {
+//     console.error(error)
+//     console.log(
+//       "You denied auto-fetching the weather based on your browser's geolocation. Check your browser settings to reset and allow this feature."
+//     )
+//   }
+// }
+// export const getLocationByPlaceName = placeName => dispatch => {
+//   try {
+//     useSWR(`/api/mapbox/${placeName}`, fetcher, {
+//       onSuccess: ({ data }) => {
+//         dispatch({
+//           type: setLocationActionTypes.SET_LOCATION,
+//           payload: {
+//             placeName,
+//             latitude,
+//             longitude,
+//             searchedTerm: capitalizeFirstLetter(placeName),
+//           },
+//         })
+//       },
+//       shouldRetryOnError: false,
+//     })
+//   } catch (error) {
+//     console.error('Error during MAPBOX fetch: ', error)
+//   }
+// }
+
+export const getLocationByCoords = coords => dispatch => {
+  axios
+    .get(`/api/mapbox/${coords[0]}/${coords[1]}`)
+    .then(res => {
+      dispatch({
+        type: setLocationActionTypes.SET_LOCATION,
+        payload: res.data,
+      })
     })
-  } catch (error) {
-    console.error(error)
-    console.log(
-      "You denied auto-fetching the weather based on your browser's geolocation. Check your browser settings to reset and allow this feature."
-    )
-  }
+    .catch(error => console.log('getLocationByCoords ERROR', error.message))
 }
-export const setLocationByPlaceName = placeName => dispatch => {
-  try {
-    useSWR(`/api/mapbox/${placeName}`, fetcher, {
-      onSuccess: ({ data }) => {
-        dispatch({
-          type: setLocationActionTypes.SET_LOCATION,
-          payload: {
-            placeName,
-            latitude,
-            longitude,
-            searchedTerm: capitalizeFirstLetter(placeName),
-          },
-        })
-      },
-      shouldRetryOnError: false,
+
+export const getLocationByPlaceName = placeName => dispatch => {
+  axios
+    .get(`/api/mapbox/${placeName}`)
+    .then(res => {
+      dispatch({
+        type: setLocationActionTypes.SET_LOCATION,
+        payload: {
+          placeName: res.placeName,
+          latitude: res.latitude,
+          longitude: res.longitude,
+          searchedTerm: capitalizeFirstLetter(placeName),
+        },
+      })
     })
-  } catch (error) {
-    console.error('Error during MAPBOX fetch: ', error)
-  }
+    .catch(error => console.log('getLocationByPlaceName ERROR', error.message))
 }
--- store/weather/action.js
+++ store/weather/action.js
@@ -1,17 +1,57 @@
-import useSWR from 'swr'
-import fetcher from '../../util/fetcher'
-
-export const setWeatherActionTypes = {
-  SET_WEATHER: 'SET_WEATHER',
+import axios from 'axios'
+import { getLocationByPlaceName } from '../location/action'
+import { capitalizeFirstLetter } from '../../util/capitalizeFirstLetter'
+export const weatherActionTypes = {
+  GET_WEATHER: 'GET_WEATHER',
 }
 
-export const setWeather = coords => dispatch => {
-  useSWR(`/api/darksky/${coords[0]}/${coords[1]}`, fetcher, {
-    onSuccess: ({ data }) => {
+// export const setWeather = coords => dispatch => {
+//   useSWR(`/api/darksky/${coords[0]}/${coords[1]}`, fetcher, {
+//     onSuccess: ({ data }) => {
+//       dispatch({
+//         type: weatherActionTypes.GET_WEATHER,
+//         payload: data,
+//       })
+//     },
+//   })
+// }
+
+export const getWeatherByCoords = coords => dispatch => {
+  axios
+    .get(`/api/darksky/${coords[0]}/${coords[1]}`)
+    .then(({ data }) => {
       dispatch({
-        type: setWeatherActionTypes.SET_WEATHER,
+        type: weatherActionTypes.GET_WEATHER,
         payload: data,
       })
-    },
-  })
+    })
+    .catch(error => console.log('ERROR in getWeatherByCoords: ', error.message))
+}
+
+export const getWeather = placeName => dispatch => {
+  axios
+    .get(`/api/mapbox/${placeName}`)
+    .then(res => {
+      dispatch({
+        type: setLocationActionTypes.SET_LOCATION,
+        payload: {
+          placeName: res.placeName,
+          latitude: res.latitude,
+          longitude: res.longitude,
+          searchedTerm: capitalizeFirstLetter(placeName),
+        },
+      })
+      axios
+        .get(`/api/darksky/${coords[0]}/${coords[1]}`)
+        .then(({ data }) => {
+          dispatch({
+            type: weatherActionTypes.GET_WEATHER,
+            payload: data,
+          })
+        })
+        .catch(error =>
+          console.log('ERROR in nested getWeather: ', error.message)
+        )
+    })
+    .catch(error => console.log('ERROR in getWeather', error.message))
 }
