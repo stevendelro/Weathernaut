54225d67a4ca2b528fe5328e5d8b0e667a1d4fce
Steven Del Rosario <stevendelro@pm.me>
2020-06-01 12:42:04 -0700

    routed main pages

---
+++ pages/[location]/daily.js
@@ -0,0 +1,10 @@
+
+function daily() {
+  return (
+    <div>
+      <h1>Daily Page</h1>
+    </div>
+  )
+}
+
+export default daily
---
+++ pages/[location]/hourly.js
@@ -0,0 +1,10 @@
+
+function hourly() {
+  return (
+    <div style={{marginTop: '10rem'}}>
+      <h1>Hourly Page</h1>
+    </div>
+  )
+}
+
+export default hourly
