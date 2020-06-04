29ec0aae90d8115079fba17ee54b2274d9def661
Steven Del Rosario <stevendelro@pm.me>
2020-06-03 23:03:33 -0700

    changed primary and secondary theme colors

--- styles/theme/theme.js
+++ styles/theme/theme.js
@@ -1,14 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
+import { teal, orange } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: teal[800],
     },
     secondary: {
-      main: '#19857b',
+      main: orange[800],
     },
     error: {
       main: red.A400,
