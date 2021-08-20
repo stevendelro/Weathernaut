d32c21ece7a7a3ac65d43ef368ad79616ee21493
Steven Del Rosario <stevendelro@pm.me>
2021-08-20 09:52:30 -0500

    updated meta tags to include descriptions and an image

--- pages/_document.js
+++ pages/_document.js
@@ -6,17 +6,34 @@ import theme from '../styles/theme/theme'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name='theme-color' content={theme.palette.primary.main} />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta
+            name="description"
+            content="A Next.js weather dashboard built with React, Redux, and Recharts."
+          />
+          <meta name="title" property="og:title" content="Weathernaut" />
+          <meta property="og:type" content="website" />
+          <meta
+            name="image"
+            property="og:image"
+            content="https://priontyl.sirv.com/Images/weathernaut.png"
+          />
+          <meta
+            name="description"
+            property="og:description"
+            content="A Next.js weather dashboard built with React, Redux, and Recharts."
+          />
+          <meta name="author" content="Steven Del Rosario" />
           <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link
-            href='https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css'
-            rel='stylesheet'
+            href="https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css"
+            rel="stylesheet"
           />
         </Head>
         <body>
